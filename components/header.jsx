dcaab109d6fc1fc236edd19066991dd4b629afd8
MD Naem <121675549+naem0@users.noreply.github.com>
2025-07-17 18:10:41 +0600

    text editor

--- components/header.jsx
+++ components/header.jsx
@@ -52,7 +52,7 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center justify-between">
+      <div className="container flex h-16 items-center justify-between mx-auto">
         <div className="flex items-center gap-6 md:gap-10">
           <Link href="/" className="flex items-center space-x-2">
             {settings?.logo ? (
